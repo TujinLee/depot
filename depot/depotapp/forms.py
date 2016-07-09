6bf8f651dc5b354867dd61ad721b0a6cdd6245ec
xuehao <xuehao0618@gmail.com>
2016-07-09 22:52:27 +0800

    validate forms

--- depot/depotapp/forms.py
+++ depot/depotapp/forms.py
@@ -1,6 +1,13 @@
+# -*- coding: utf8 -*-
 from django import forms
 from models import *
+import itertools
 
+def anyTrue(predicate, sequence):
+    return True in itertools.imap(predicate, sequence)
+
+def endsWith(string, *endings):
+    return anyTrue(string.endswith, endings)
 
 class ProductForm(forms.ModelForm):
 
@@ -9,3 +16,15 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price <= 0:
+            raise forms.ValidationError("价格必须大于零")
+        return price
+
+    def clean_image_url(self):
+        url = self.cleaned_data['image_url']
+        if not endsWith(url.lower(), '.jpg', '.png', '.gif'):
+            raise forms.ValidationError('图片格式必须为jpg、png或gif')
+        return url
--- depot/depotapp/tests.py
+++ depot/depotapp/tests.py
@@ -1,11 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+#/usr/bin/python
+# -*- coding: utf8 -*-
 from django.test import TestCase
+from forms import ProductForm
 
 
 class SimpleTest(TestCase):
@@ -14,3 +10,69 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ProductTest(TestCase):
+    def setUp(self):
+        self.product = {
+            'title':'My Book Title',
+            'description':'yyy',
+            'image_url':'https://img3.doubanio.com/mpic/s1727290.jpg',
+            'price':1
+        }
+
+        f = ProductForm(self.product)
+        f.save()
+        self.product['title'] = 'My Another Book Title'
+
+    def test_attrs_cannot_empty(self):
+        '''title，description，price，image_url不能为空
+        '''
+        f = ProductForm({})
+        self.assertFalse(f.is_valid())
+        self.assertTrue(f['title'].errors)
+        self.assertTrue(f['description'].errors)
+        self.assertTrue(f['price'].errors)
+        self.assertTrue(f['image_url'].errors)
+
+    def test_price_positive(self):
+        '''price必须大于零
+        '''
+        f = ProductForm(self.product)
+        self.assertTrue(f.is_valid())
+
+        self.product['price'] = 0
+        f = ProductForm(self.product)
+        self.assertFalse(f.is_valid())
+
+        self.product['price'] = -1
+        f = ProductForm(self.product)
+        self.assertFalse(f.is_valid())
+
+        self.product['price'] = 1
+
+    def test_imgae_url_endwiths(self):
+        '''image_url必须以jpg，png，jpg结尾，并且对大小写不敏感
+        '''
+        url_base = 'http://www.baidu.com/'
+        oks = ('fred.gif', 'fred.jpg', 'fred.png', 'FRED.JPG', 'FRED.Jpg')
+        bads = ('fred.doc', 'fred.gif/more', 'fred.gif.more')
+        for endwith in oks:
+            self.product['image_url'] = url_base+endwith
+            f = ProductForm(self.product)
+            self.assertTrue(f.is_valid(),msg='error when image_url endwith '+endwith)
+
+        for endwith in bads:
+            self.product['image_url'] = url_base+endwith
+            f = ProductForm(self.product)
+            self.assertFalse(f.is_valid(),msg='error when image_url endwith '+endwith)
+
+        self.product['image_url'] = 'https://img3.doubanio.com/mpic/s1727290.jpg'
+
+    def test_title_unique(self):
+        '''titile必须唯一
+        '''
+        self.product['title'] = 'My Book Title'
+        f = ProductForm(self.product)
+        self.assertFalse(f.is_valid())
+        self.product['title'] = 'My Another Book Title'
